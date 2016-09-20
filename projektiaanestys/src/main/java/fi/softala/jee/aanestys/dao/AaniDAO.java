c3753a2b59bb4d48085e983c7c7ce2de5db0dc2f
Joel <a1500908@myy.haaga-helia.fi>
2016-09-20 10:25:54 +0300

    ÄäniDAO

--- projektiaanestys/src/main/java/fi/softala/jee/aanestys/dao/AaniDAO.java
+++ projektiaanestys/src/main/java/fi/softala/jee/aanestys/dao/AaniDAO.java
@@ -1,5 +1,18 @@
 package fi.softala.jee.aanestys.dao;
 
+import java.util.List;
+
+import fi.softala.jee.aanestys.bean.Aani;
+
+
 public interface AaniDAO {
 
+	public void insert (Aani Aani);
+	
+	public void delete (int AaniID);
+	
+	public Aani get (int Aani);
+	
+	public List<Aani> lista();
+	
 }
---
+++ projektiaanestys/src/main/java/fi/softala/jee/aanestys/dao/AaniDAOImpl.java
@@ -0,0 +1,29 @@
+package fi.softala.jee.aanestys.dao;
+
+import java.util.List;
+
+import fi.softala.jee.aanestys.bean.Aani;
+
+public class AaniDAOImpl implements AaniDAO{
+
+	public void insert(Aani Aani) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void delete(int AaniID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Aani get(int Aani) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Aani> lista() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
