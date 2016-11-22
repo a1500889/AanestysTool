6c43f3c65622133146e96fdff12edffa41586ae8
ossi <ossi.palander@myy.haaga-helia.fi>
2016-11-22 13:36:10 +0200

    erropagesettiä

--- projektiaanestys/src/main/java/fi/softala/jee/aanestys/control/MainController.java
+++ projektiaanestys/src/main/java/fi/softala/jee/aanestys/control/MainController.java
@@ -364,5 +364,27 @@ public class MainController {
  
 	}
 	
+	 String path = "/error";
+	 
+	 @RequestMapping(value="/400")
+	 public String error400(){
+	  System.out.println("custom error handler");
+	  return path+"/400";
+	 }
+	 
+	 @RequestMapping(value="/404")
+	 public String error404(){
+	  System.out.println("custom error handler");
+	  return path+"/404";
+	 }
+	 
+	 @RequestMapping(value="/500")
+	 public String error500(){
+	  System.out.println("custom error handler");
+	  return path+"/500";
+	 }
+	 
+	 
+	}
 	
-}
+
